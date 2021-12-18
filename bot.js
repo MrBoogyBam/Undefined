933ad589b2f794ebf06234fd2c6905a03a43433d
MrBoogyBam <mrboogybambusiness@gmail.com>
2021-12-18 11:58:43 +0200

    Added command handler

--- bot.js
+++ bot.js
@@ -3,6 +3,30 @@ const bot = new Client({ intents: [ Intents.FLAGS.GUILDS, "GUILD_MESSAGES", "GUI
 const { token, prefix } = require('./config.json');
 const fs = require('fs');
 
+bot.commands = new Collection();
+
+fs.readdirSync('./src/commands').forEach(dir => {
+    fs.readdir(`./commands/${dir}`, (err, files) => {
+        if(err) throw err;
+
+        let jsFiles = files.filter(f => f.split(".").pop() == "js");
+
+        if(jsFiles.length <= 0) return console.log("[COMMAND HANDLER]: Can't find any commands.");
+
+        jsFiles.forEach(file => {
+            let fileGet = require(`./commands/${dir}/${file}`);
+
+            console.log(`[COMMAND HANDLER]: File ${file} was loaded.`);
+
+            try {
+                bot.commands.set(fileGet.help.name, fileGet);
+            } catch (err) {
+                return console.log(err);
+            }
+        });
+    });
+});
+
 bot.on('ready', () => {
     console.log(`${bot.user.username} is now online!`);
 });

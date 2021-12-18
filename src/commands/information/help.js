1ebb539a6d11929f9f48d9e7a9e367ecd632fd6c
MrBoogyBam <mrboogybambusiness@gmail.com>
2021-12-18 18:31:38 +0200

    Added kick command to help command

--- src/commands/information/help.js
+++ src/commands/information/help.js
@@ -5,9 +5,10 @@ module.exports.run = async(bot, message, prefix) => {
     if(message.content.toLowerCase() == `${prefix}help`) {
         let helpEmbed = new MessageEmbed()
             .setTitle(`__${bot.user.username} Commands__`)
-            .setDescription(`This is a list of all command categories for ${bot.user.username} bot, if you'd like to see the commands in each category, use the command \`${prefix}help category\`.`)
+            .setDescription(`This is a list of all command categories for ${bot.user.username} Bot, if you'd like to see the commands in each category, use the command \`${prefix}help category\`.`)
             .setFields(
-                { name: "Information", value: "List of all informative commands" }
+                { name: "Information", value: "List of all informative commands" },
+                { name: "Moderation", value: "List of all moderation commands" }
             )
             .setColor('#008cf7')
             .setFooter(`${bot.user.username} Bot`, bot.user.displayAvatarURL());
@@ -20,14 +21,26 @@ module.exports.run = async(bot, message, prefix) => {
     if(message.content.toLowerCase() == `${prefix}help information`) {
         let infoEmbed = new MessageEmbed()
             .setTitle(`__${bot.user.username} Informative Commands__`)
-            .setDescription(`This is a list of all informative commands for ${bot.user.username} bot, if you'd like to see more information about each command, use the command \`${prefix}help command\``)
+            .setDescription(`This is a list of all informative commands for ${bot.user.username} Bot, if you'd like to see more information about each command, use the command \`${prefix}help command\``)
             .setFields(
-                { name: "ping", value: "Shows the bot's latency" }
+                { name: "Ping", value: "Shows the bot's latency" }
             )
             .setColor('#008cf7')
             .setFooter(`${bot.user.username} Bot`, bot.user.displayAvatarURL());
 
         message.channel.send({ embeds: [infoEmbed] });
+        return;
+    } else if(message.content.toLowerCase() == `${prefix}help moderation`) {
+        let modHelp = new MessageEmbed()
+            .setTitle(`__${bot.user.username} Moderation Commands__`)
+            .setDescription(`This is a list of all moderation commands for ${bot.user.username} Bot, if you'd like to see more information about each command, use the command \`${prefix}help command\``)
+            .setFields(
+                { name: "Kick", value: "Kicks mentioned user" }
+            )
+            .setColor('#008cf7')
+            .setFooter(`${bot.user.username} Bot`, bot.user.displayAvatarURL());
+
+        message.channel.send({ embeds: [modHelp] });
         return;
     }
 
@@ -37,12 +50,24 @@ module.exports.run = async(bot, message, prefix) => {
             .setTitle(`__${bot.user.username} Ping Command__`)
             .setDescription(`The bot will reply with it's latency and the API latency.`)
             .setFields(
-                { name: "Usage:", value: `${prefix}ping` }
+                { name: "Usage:", value: `\`${prefix}ping\`` }
             )
             .setColor('#008cf7')
             .setFooter(`${bot.user.username} Bot`, bot.user.displayAvatarURL());
 
         message.channel.send({ embeds: [pingEmbed] });
+        return;
+    } else if(message.content.toLowerCase() == `${prefix}help kick`) {
+        let kickEmbed = new MessageEmbed()
+            .setTitle(`__${bot.user.username} Kick Command__`)
+            .setDescription(`The bot will kick the first member you mention in your message.`)
+            .setFields(
+                { name: "Usage:", value: `\`${prefix}kick user\`` }
+            )
+            .setColor('#008cf7')
+            .setFooter(`${bot.user.username} Bot`, bot.user.displayAvatarURL());
+
+        message.channel.send({ embeds: [kickEmbed] });
         return;
     }
 }
